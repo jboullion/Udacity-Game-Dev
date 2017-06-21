076917a0ee3c21e19ae521ab3390ee4167cff5fb
jboullion <jboullion83@gmail.com>
2017-06-20 21:31:10 -0500

    Broken?
    The example code is not working and the xhr.onload doesn't seem to be
    passing the right information. Not sure what the fix yet

--- xhr.js
+++ xhr.js
@@ -2,8 +2,17 @@
 
 function xhrGet(reqUri,callback) {
 	var xhr = new XMLHttpRequest();
-
-	xhr.open("GET", reqUri, true);
 	xhr.onload = callback;
+	xhr.open("GET", reqUri, true);
+
+	xhr.onreadystatechange = function (aEvt) {
+	  if (xhr.readyState == 4) {
+	     if(xhr.status == 200){
+		      console.log(xhr.responseText);
+		  }else{
+		      alert("Error loading page\n");
+		  }
+	  }
+	};
 	xhr.send();
 }
